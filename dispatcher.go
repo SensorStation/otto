d68a27741789998e6f46c45eec7397d89e9505a8
Rusty Eddy <rusty@rustyeddy.com>
2023-10-21 09:41:18 -0700

    Added the dispatcher

---
+++ dispatcher.go
@@ -0,0 +1,66 @@
+package main
+
+import "log"
+
+type dispatcher struct {
+	InQ    chan *Msg
+	StoreQ *chan *Msg
+	webQ   map[chan *Msg]chan *Msg
+}
+
+func (d *dispatcher) addWebQ() chan *Msg {
+	c := make(chan *Msg)
+	d.webQ[c] = c
+	return c
+}
+
+func (d *dispatcher) freeWebQ(c chan *Msg) {
+	delete(d.webQ, c)
+	close(c)
+}
+
+func (d *dispatcher) addStoreQ() *chan *Msg {
+	c := make(chan *Msg)
+	d.StoreQ = &c
+	return d.StoreQ
+}
+
+func newDispatcher() (d *dispatcher) {
+	d = &dispatcher{}
+	d.InQ = make(chan *Msg)
+
+	go func() {
+
+		for true {
+			select {
+			case msg := <-d.InQ:
+				log.Printf("[I] %s", msg.String())
+
+				src := msg.Source
+				switch msg.Category {
+				case "data":
+
+					// if there are websockets waiting to recieve this
+					// data send it to them
+					if d.StoreQ != nil {
+						*d.StoreQ <- msg
+					}
+
+					for c, _ := range d.webQ {
+						c <- msg
+					}
+
+				case "control":
+					log.Println("Do something with the control from ", src)
+
+				default:
+					log.Println("Uknonwn message type: ", msg.Device)
+				}
+
+			}
+		}
+
+	}()
+
+	return d
+}
