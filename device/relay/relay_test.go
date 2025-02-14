c8e6e85501ec9fdf5c7144ae1f397fef64856810
Rusty Eddy <rusty@rustyeddy.com>
2025-02-13 20:22:01 -0800

    added to relay tests also

--- device/relay/relay_test.go
+++ device/relay/relay_test.go
@@ -1,10 +1,39 @@
 package relay
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/sensorstation/otto/device"
+	"github.com/sensorstation/otto/messanger"
+)
 
 func TestRelay(t *testing.T) {
-	r := New("relay", 5)
-	if r.Name() != "relay" {
-		t.Errorf("relay expected Name (%s) got (%s)", "relay", r.Name())
+	device.Mock(true)
+
+	relay := New("relay", 5)
+	if relay.Name() != "relay" {
+		t.Errorf("relay expected Name (%s) got (%s)", "relay", relay.Name())
+	}
+
+	msg := messanger.New(relay.GetPub(), []byte("on"), "test")
+	relay.Callback(msg)
+
+	v, err := relay.Value()
+	if err != nil {
+		t.Fatalf("relay.Value() got error %v", err)
+	}
+	if v != 1 {
+		t.Errorf("relay expected (1) got (%d)", v)
+	}
+
+	msg = messanger.New(relay.GetPub(), []byte("off"), "test")
+	relay.Callback(msg)
+
+	v, err = relay.Value()
+	if err != nil {
+		t.Fatalf("relay.Value() got error %v", err)
+	}
+	if v != 0 {
+		t.Errorf("relay expected (0) got (%d)", v)
 	}
 }
