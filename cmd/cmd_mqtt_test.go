3454aae54fe4d56cd6bc0b5434840e588dfb6460
Rusty Eddy <rusty@rustyeddy.com>
2025-02-26 20:29:16 -0800

    debugging template web interface

---
+++ cmd/cmd_mqtt_test.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/sensorstation/otto/messanger"
+	"github.com/spf13/cobra"
+)
+
+func TestMqttCmd(t *testing.T) {
+	cmd := mqttCmd
+	if cmd == nil {
+		t.Fatal("mqttCmd is nil")
+	}
+	if cmd.Use != "mqtt" {
+		t.Errorf("expected Use to be 'mqtt', got %s", cmd.Use)
+	}
+	if cmd.Short != "Configure and interact with MQTT broker" {
+		t.Errorf("expected Short to be 'Configure and interact with MQTT broker', got %s", cmd.Short)
+	}
+	if cmd.Run == nil {
+		t.Errorf("expected Run to be defined, got nil")
+	}
+}
+
+func TestMqttRun(t *testing.T) {
+	mqttConfig.Broker = "localhost"
+	cmd := &cobra.Command{}
+	args := []string{}
+
+	mqttRun(cmd, args)
+
+	m := messanger.GetMQTT()
+	if m.Broker != "localhost" {
+		t.Errorf("expected Broker to be 'localhost', got %s", m.Broker)
+	}
+	if !m.IsConnected() {
+		t.Error("expected MQTT to be connected")
+	}
+}
